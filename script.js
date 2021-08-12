255aab6670505d20f561caa4f3dd4b17f9ef1c23
AnthonyMerive <anthonycolmenaresrivas@gmail.com>
2021-08-12 17:51:01 -0500

    primeros pasos

---
+++ script.js
@@ -0,0 +1,5 @@
+const formulario = document.querySelector('#form');
+
+let divisa = document.querySelector('#monedaSelected');
+let divisab = document.querySelector('#monedaConvert');
+let result = document.querySelector('#result');
